947202b42212a0b17a8c93235d0c3b16ad942d3d
laziza@mail.ru <laziza00>
2022-03-07 09:04:37 +0500

    initial commit

---
+++ integer.js
@@ -0,0 +1,473 @@
+
+
+// ===============1 masla
+
+// let a= +prompt();
+// for (let i =0; i<=a; i++) {
+//     if (a%i==0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// ===============2 masla
+
+// let a= +prompt();
+// let b =0;
+// for (let i =0; i<=a; i++) {
+//     if (a%i==0) {
+//         b += i;
+//     }
+// }
+// console.log(b);
+
+// ===============3 masla
+
+// let a = 28;
+// let b = 0;
+// for (let i =0; i<a; i++) {
+//     if (a%i==0) {
+//         b += i;
+//     }
+// }
+//     if(b==a){
+//     console.log("mukkammal son");
+// }
+//  else {
+//      console.log("muakkaml emas");
+//  }
+
+
+// ===============4 masla
+
+// let a = 1000;
+// let result = 0;
+
+// for (let i =1; i<a; i++) {
+//     let b = 0
+//     for (let j =1; j<i; j++) {
+//         if (i%j==0) {
+//             b += j;
+//         }
+//     }
+//         if(b==i){
+//         console.log(b);
+//         result++;
+//     }
+// }
+// console.log(result, ' ta mukammal son');
+
+
+
+
+    
+
+
+// ===============5 masla
+
+
+// let a = +prompt();
+// for (let i = 0; i<=a; i++) {
+//     if (i%3==0 && i%5!= 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// ===============6 masla
+
+
+// let a = +prompt();
+// let b =1
+// for (let i = 0; i<=a; i++) {
+//     if (a%i!=0) {
+//         b++;
+//     }
+// }
+//  if (b==a) {
+//      console.log("tub son");
+//  }
+//   else {
+//       console.log("tub emas");
+//   }
+
+
+// ===============7 masla
+
+
+// let a = 21;
+// let b =1;
+// for (let i = 2; i<=a; i++) {
+//     tub_son = true;
+//     for (let j = 2; j<i; j++) {
+//          if (i%j==0){
+//              tub_son = false;
+//              break;
+//          }
+//     }
+//     if (tub_son==true)
+//     console.log(i);
+// }
+
+
+// ===============8 masla
+
+// let limit = 25;
+// let a, b, c = 0;
+// let m = 2;
+// while (c < limit)
+//     {
+//     for(let n = 1; n < m; n++){
+//         a = m * m - n * n;
+//         b = 2 * m * n;
+//         c = m * m + n * n;
+//         if (c > limit)
+//             break;
+//             console.log(a + " " + b + " " + c);
+//     }
+//     m++;
+// }
+
+
+// ===============9 masla
+
+// let f = 100
+// let son1 = 220;
+// let a =0
+// let b =0
+// let son2 = 284;
+
+// for (let k =0; k<=f; k++) {
+//     for (let i =0; i<=son1; i++) {
+//         if (son1%i==0) {
+//             a+= i;
+//         }
+//         console.log(a);
+//     }
+//     for (let j =0; j<=son2; j++) {
+//         if (son2%j==0) {
+//             b+= j;
+//         }
+//         console.log(b);
+//     }
+// }
+// if (a==b) {
+//     console.log(a);
+// }
+
+
+// ===============10 masla
+
+
+// let n =25;
+// let a =0;
+
+// for (let i=0; i<=n; i++){
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//===================== if masala 11
+
+// let a = 7;
+// let b = 3;
+
+// if (a%2 !=0 || b%2!= 0) {
+//     console.log("True");
+// }
+//  else {
+//      console.log("false");
+//  }
+
+
+
+//===================== if masala 12
+
+// let a = +prompt();
+// let c= 0
+// while (a>0){
+
+//     b = a%10;
+//     a = Math.floor(a/10)
+//     c++
+// }
+// console.log(c, "honali son");
+
+
+//===================== if masala 13
+
+// let a = +prompt();
+// let c = 0
+// while(a>0) {
+//     let b = 0;
+//     b = a%10
+//     a = Math.floor(a / 10);
+//     c++
+// }
+// if (c==3) {
+//     console.log("True");
+// }
+// else {
+//     console.log("false");
+//  }
+
+
+//===================== if masala 14
+
+// let a = 22;
+// let c = 0
+// while(a>0) {
+//     let b = 0;
+//     b = a%10
+//     a = Math.floor(a /= 10);
+//     c++
+// }
+// if (c==2 && a.typeof()) {
+//     console.log("True");
+// }
+// else {
+//     console.log("false");
+//  }
+
+
+//===================== if masala 15
+
+// let a = 20;
+// let c = 0;
+// let sum =0
+// while(a>0) {
+//     let b = 0;
+//     b = a%10
+//     a = Math.floor(a /= 10);
+//     c++;
+//     sum += b
+// }
+// if (c==2 && sum %2==0) {
+//     console.log("True");
+// }
+// else {
+//     console.log("false");
+//  }
+
+
+//===================== if masala 16
+
+// let a = 21;
+// let c = 0;
+// let sum =0
+// while(a>0) {
+//     let b = 0;
+//     b = a%10
+//     a = Math.floor(a /= 10);
+//     c++;
+//     sum += b
+// }
+// if (c==2 && sum %2==1) {
+//     console.log("True");
+// }
+// else {
+//     console.log("false");
+//  }
+
+
+//===================== if masala 17
+
+// let a = 101;
+// let c = 0;
+// let sum =0
+// while(a>0) {
+//     let b = 0;
+//     b = a%10
+//     a = Math.floor(a /= 10);
+//     c++;
+//     sum += b
+// }
+// if (c==3 && sum %2==1) {
+//     console.log("True");
+// }
+// else {
+//     console.log("false");
+//  }
+
+
+//===================== if masala 18
+
+// let a = 1017;
+// let c = 0;
+// while(a>0) {
+//     let b = 0;
+//     b = a%10
+//     a = Math.floor(a /= 10);
+//     c++;
+// }
+// if (c==5 ) {
+//     console.log("True");
+// }
+// else {
+//     console.log("false");
+//  }
+
+
+//===================== if masala 21
+
+// let a = -9;
+// let c = 0;
+// if (a> 0 ) {
+//     a += 1;
+//     console.log(a);
+// }
+// else {
+//     console.log(a);
+//  }
+
+//===================== if masala 22
+
+// let a = 10;
+// let c = 0;
+// if (a> 0 ) {
+//     a += 1;
+//     console.log(a);
+// }
+// else {
+//   a -= 2
+//     console.log(a);
+//  }
+
+
+//===================== if masala 23
+
+// let a = -2;
+// let c = 0;
+// if (a> 0 ) {
+//     a += 1;
+//     console.log(a);
+// }
+// else if (a==0) {
+//   a = 10;
+//   console.log(a);
+// }
+// else if (a<0){
+//   a -= 2
+//     console.log(a);
+//  }
+
+
+//===================== if masala 26
+
+// let a = 12;
+// let b =30;
+// let min = Math.min(a, b);
+// let max = Math.max (a, b);
+// console.log(`eng katt son ${max} eng kicchik son ${min}`);
+
+
+
+
+
+
+
+
+
+
+
+// ================funksiya masala
+
+
+
+
+// ====================funksiya 11 masala
+
+// function miniMax(a, b, c, d){
+//   let x = Math.max(a, b, c ,d);
+//   let y = Math.min(a, b, c, d);
+
+//   return `Eng katta son ${x}. Eng kichik son ${y}`
+// }
+
+// console.log(miniMax(20, 0, 34, -3));
+
+// ====================funksiya 12 masala
+
+// function sort(a, b, c){
+//   let max = Math.max(a, b, c);
+//   let min = Math.min(a, b, c);
+
+//   if (a<max && a>min){
+//     console.log(min, a, max);
+//   }
+//   else if (b<max && b>min) {
+//     console.log(min, b, max);
+//   }
+//   else {
+//     console.log(min, c, max);
+//   }
+
+// }
+
+// console.log(sort(20, 0, 34));
+
+
+// ====================funksiya 13 masala
+
+// function sort(a, b, c){
+//   let max = Math.max(a, b, c);
+//   let min = Math.min(a, b, c);
+
+//   if (a<max && a>min){
+//     console.log(max, a, min);
+//   }
+//   else if (b<max && b>min) {
+//     console.log(max, a, min);
+//   }
+//   else {
+//     console.log(max, a, min);
+//   }
+
+// }
+
+// console.log(sort(20, 0, 34));
+
+
+// ====================funksiya 14 masala
+
+// function sort(a, b, c){
+
+// let a1 = b;
+// let b1 = c;
+// let c1 = a;
+
+// }
+
+// console.log(sort(20, 0, 34));
+
+
+// ====================funksiya 16 masala
+
+// function qaytar(a){
+//   if (a>0) {
+//     return 1;
+//   }
+//   else if (a==0) {
+//     return 0
+//   }
+//   else if (a<0) {
+//     return -1
+//   }
+// }
+
+// console.log(qaytar(-9));
